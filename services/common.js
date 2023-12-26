1d502c218d11b335fbf16b747b6eb91baa3ed7d1
FerdousMahmudChowdhury <ferdous.m.chowdhury@gmail.com>
2023-12-26 16:27:20 +0600

    Backend Part 10 - Email and Reset Password Flow

--- services/common.js
+++ services/common.js
@@ -1,5 +1,18 @@
 const passport = require('passport');
 
+const nodemailer = require('nodemailer');
+
+let transporter = nodemailer.createTransport({
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false, // true for 465, false for other ports
+  auth: {
+    user: 'coderdost@gmail.com', // gmail
+    pass: process.env.MAIL_PASSWORD, // pass
+  },
+});
+
+
 exports.isAuth = (req, res, done) => {
   return passport.authenticate('jwt');
 };
@@ -18,3 +31,15 @@ exports.cookieExtractor = function (req) {
   // token="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY1ODVlZWVmNmZlZmVhMDM2YjkwZmU3ZCIsInJvbGUiOiJ1c2VyIiwiaWF0IjoxNzAzMjc2NDQxfQ.QW9cpMkBZAFbRt1fx8NP8hHWC-OR8-eb9nbh--rw2Po"
   return token;
 };
+
+
+exports.sendMail = async function ({to, subject, text, html}){
+  let info = await transporter.sendMail({
+      from: '"E-commerce" <coderdost@gmail.com>', // sender address
+      to,
+      subject,
+      text,
+      html
+    });
+  return info;  
+}
